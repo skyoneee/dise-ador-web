86ff04f331a19b0b52f0c8813494acfa20f967c9
user-daw-zayas <javier.perezarteaga@educa.madrid.org>
2013-11-25 14:22:01 +0100

    crea divs sin fallos :D

--- www/crea/js/init.js
+++ www/crea/js/init.js
@@ -8,27 +8,61 @@
     window.onload = function() {
 
         document.getElementById('w').onkeypress = function(event) {
-			ancho= event.target.value;
+			if (event.keyCode === 13) {
+				this.blur();
+            }
+			w.addEventListener('blur', function() {
+			   ancho= event.target.value;
+			});
         };
 		
 		document.getElementById('h').onkeypress = function(event) {
-			alto= event.target.value;
+			if (event.keyCode === 13) {
+				this.blur();
+            }
+			h.addEventListener('blur', function() {
+			   alto= event.target.value;
+			});
         };
 		
 		document.getElementById('bg').onkeypress = function(event) {
-			bgdiv= event.target.value;
+			if (event.keyCode === 13) {
+				this.blur();
+            }
+			bg.addEventListener('blur', function() {
+			   bgdiv= event.target.value;
+			});
+			
         };
 		
 		document.getElementById('b').onkeypress = function(event) {
-			borde= event.target.value;
+			if (event.keyCode === 13) {
+				this.blur();
+            }
+			b.addEventListener('blur', function() {
+			   borde= event.target.value;
+			});
+			
         };
 		
 		document.getElementById('m').onkeypress = function(event) {
-			margen= event.target.value;
+			if (event.keyCode === 13) {
+				this.blur();
+            }
+			m.addEventListener('blur', function() {
+			   margen= event.target.value;
+			});
+			
         };
 		
 		document.getElementById('p').onkeypress = function(event) {
-			pad= event.target.value;
+			if (event.keyCode === 13) {
+				this.blur();
+            }
+			p.addEventListener('blur', function() {
+			   pad= event.target.value;
+			});
+			
         };
 
         document.getElementById('enviar').onclick = function() {
@@ -38,13 +72,6 @@
     };
 
     function addDiv(ancho,alto,bgdiv,borde,margen,pad) {
-			debugger
-				console.log(ancho);
-							console.log(alto);
-										console.log(bgdiv);
-				console.log(borde);
-							console.log(margen);
-										console.log(pad);
             var divUI = new DIV_APP.divsUI(ancho,alto,bgdiv,borde,margen,pad, parent);
             dUI.push(divUI);
             document.getElementById('main').appendChild(divUI.container);
