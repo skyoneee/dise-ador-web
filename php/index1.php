af9b56bbd76cba2fb908b281468596b759185785
user-daw-zayas <javier.perezarteaga@educa.madrid.org>
2013-11-27 10:08:42 +0100

    sin readme

---
+++ php/index1.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DW</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<link rel="stylesheet" href="css/foundation.min.css" />
+	<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<div id="top">
+	<div class="row">
+		<div class="large-12 columns">
+			<nav class="top-bar">
+				<ul class="title-area">
+					<li class="name logo">
+						<a href="#"><img src="img/logo.png" height="60" width="150"></a>
+					</li>
+					<li class="toggle-topbar menu-icon"><a href="#"><span>Menú</span></a></li>
+				</ul>
+				<section class="top-bar-section">
+					<ul class="right" id="menu">
+						<li><a href="login.html">Registrar/Login</a></li>
+						<li><a href="">Conf</a></li>
+					</ul>
+				</section>
+			</nav>
+		</div>
+	</div>
+</div>
+
+<header id="header">
+	<div class="row">
+		<div class="large-8 columns">
+			<h1>Diseñador Web</h1>
+			<span class="subheading">Tu web, fácil y cómoda</span>
+		</div>	
+	</div>
+</header>
+
+<div id="features" class="section features">
+	<div class="row">
+		<div class="large-4 columns feature">
+			<a href="tusficheros.html"><span class="icon hi-icon"><img src="img/crear.png" height="100" width="100"></span></a>
+			<h3>Tus páginas HTML y CSS</h3>
+		</div>
+		<div class="large-4 columns feature">
+			<a href="crea/index.html"><span class="icon hi-icon"><img src="img/mod.png" height="100" width="100"></span></a>
+			<h3>Crear y editar HTML y CSS</h3>
+		</div>
+		<div class="large-4 columns feature">
+			<a href="aplicaciones.html"><span class="icon hi-icon"><img src="img/datos.png" height="100" width="100"></span></a>
+			<h3>Aplicaciones para tu página</h3>
+		</div>
+	</div>
+</div>
+
+<div id="pricing-table" class="section pricing gray">
+	<div class="row">
+		<div class="large-12 columns">
+			<ul data-orbit>
+				<li class="quote">
+					<blockquote>Elige entre múltiples plantillas.</blockquote>
+				</li>
+				<li class="quote">
+					<blockquote>Modifícalas a tu gusto.</blockquote>
+				</li>
+				<li class="quote">
+					<blockquote>Publica tu página.</blockquote>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<div class="section paper">
+	<img src="img/borde.png">
+</div>
+
+<div id="pricing-table" class="section pricing gray2">
+	<div class="row price-table">
+		<div class="large-4 columns">
+			<ul class="pricing-table">
+				<li class="description"><img src="img/template1.jpg"></li>
+				<li class="cta-button"><a class="button secondary" href="#">Editar</a></li>
+			</ul>
+		</div>
+		<div class="large-4 columns">
+			<ul class="pricing-table">
+				<li class="description"><img src="img/template2.jpg"></li>
+				<li class="cta-button"><a class="button secondary" href="#">Editar</a></li>
+			</ul>
+		</div>
+		<div class="large-4 columns">
+			<ul class="pricing-table">
+				<li class="description"><img src="img/template3.jpg"></li>
+				<li class="cta-button"><a class="button secondary" href="#">Editar</a></li>
+			</ul>
+		</div>
+		<div class="large-4 columns">
+			<ul class="pricing-table">
+				<li class="description"><img src="img/template2.jpg"></li>
+				<li class="cta-button"><a class="button secondary" href="#">Editar</a></li>
+			</ul>
+		</div>
+		<div class="large-4 columns">
+			<ul class="pricing-table">
+				<li class="description"><img src="img/template3.jpg"></li>
+				<li class="cta-button"><a class="button secondary" href="#">Editar</a></li>
+			</ul>
+		</div>
+		<div class="large-4 columns">
+			<ul class="pricing-table">
+				<li class="description"><img src="img/template1.jpg"></li>
+				<li class="cta-button"><a class="button secondary" href="#">Editar</a></li>
+			</ul>
+		</div>
+		<div class="large-4 columns">
+			<ul class="pricing-table">
+				<li class="description"><img src="img/template3.jpg"></li>
+				<li class="cta-button"><a class="button secondary" href="#">Editar</a></li>
+			</ul>
+		</div>
+		<div class="large-4 columns">
+			<ul class="pricing-table">
+				<li class="description"><img src="img/template1.jpg"></li>
+				<li class="cta-button"><a class="button secondary" href="#">Editar</a></li>
+			</ul>
+		</div>
+		<div class="large-4 columns">
+			<ul class="pricing-table">
+				<li class="description"><img src="img/template2.jpg"></li>
+				<li class="cta-button"><a class="button secondary" href="#">Editar</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<footer>
+	<div class="row">
+		<div class="large-6 columns">
+			<ul class="inline-list left">
+				<li>2013 &copy; Diseñador Web</a></li>
+			</ul>
+		</div>
+		<div class="large-6 columns">
+			<ul class="inline-list right">
+				<li><a href="http://www.facebook.com" class="icon icon-facebook"></a></li>
+				<li><a href="http://twitter.com" class="icon icon-twitter"></a></li>
+				<li><a href="https://plus.google.com" class="icon icon-googleplus"></a></li>
+			</ul>
+		</div>
+	</div>
+</footer>
+
+<script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
+<script type="text/javascript" src="js/foundation.min.js"></script>
+<script type="text/javascript" src="js/custom.js"></script>
+
+</body>
+</html>
