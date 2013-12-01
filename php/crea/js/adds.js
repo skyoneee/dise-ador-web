e8ee66f17ebe52df9f94b5feb902a3f5a7d880f8
user-daw-zayas <javier.perezarteaga@educa.madrid.org>
2013-12-01 22:44:45 +0100

    añade img

--- php/crea/js/adds.js
+++ php/crea/js/adds.js
@@ -12,11 +12,13 @@
         }
 		
 		function modDiv(div) {
-		    div.style.border = '3px solid blue';
             document.getElementById('w').value= String(div.style.width);
 			document.getElementById('h').value= String(div.style.height);
 			document.getElementById('bg').value= String(div.style.background);
-			var ancho,alto,bgdiv;
+			document.getElementById('b').value= String(div.style.border);
+			document.getElementById('m').value= String(div.style.margin);
+			document.getElementById('p').value= String(div.style.padding);
+			var ancho,alto,bgdiv,borde,margen,pad;
 			document.getElementById('w').onkeypress = function(event) {
 				if (event.keyCode === 13) {
 					this.blur();
@@ -42,31 +44,102 @@
 				});
 				
 			};	
+			document.getElementById('b').onkeypress = function(event) {
+				if (event.keyCode === 13) {
+					this.blur();
+				}
+				bg.addEventListener('blur', function() {
+				   borde= event.target.value;
+				});
+				
+			};
+			document.getElementById('m').onkeypress = function(event) {
+				if (event.keyCode === 13) {
+					this.blur();
+				}
+				bg.addEventListener('blur', function() {
+				   margen= event.target.value;
+				});
+				
+			};
+			document.getElementById('p').onkeypress = function(event) {
+				if (event.keyCode === 13) {
+					this.blur();
+				}
+				bg.addEventListener('blur', function() {
+				   pad= event.target.value;
+				});
+				
+			};
+
+			
 			document.getElementById('enviar').onclick = function() {
 				div.style.width = ancho;
 				div.style.height = alto;
 				div.style.background = bgdiv;
+				div.style.border = borde;
+				div.style.margin = margen;
+				div.style.padding = pad;
 			};
 
         }
-		
+		function modImg(div) {
+            document.getElementById('w1').value= String(div.style.width);
+			document.getElementById('h1').value= String(div.style.height);
+			document.getElementById('img').value= String(div.innerHTML);
+			var anchoi,altoi,urlimg;
+			document.getElementById('w').onkeypress = function(event) {
+				if (event.keyCode === 13) {
+					this.blur();
+				}
+				w.addEventListener('blur', function() {
+				   anchoi= event.target.value;
+				});
+			};
+			document.getElementById('h').onkeypress = function(event) {
+				if (event.keyCode === 13) {
+					this.blur();
+				}
+				h.addEventListener('blur', function() {
+				   altoi= event.target.value;
+				});
+			};
+			document.getElementById('img').onkeypress = function(event) {
+				if (event.keyCode === 13) {
+					this.blur();
+				}
+				bg.addEventListener('blur', function() {
+				   urlimg= event.target.value;
+				});
+				
+			};	
+			document.getElementById('enviar1').onclick = function() {
+				div.style.width = anchoi;
+				div.style.height = altoi;
+				div.innerHTML = urlimg;
+			};
+
+        }
 		function delDiv(div) {
 			div.parentNode.removeChild(div);
 		}
 
         document.getElementById('div').onclick = function() {
+		    document.getElementById('header2').style.display = 'block';
+			document.getElementById('header3').style.display = 'none';
 			var main = document.getElementById('main');
 			var div  = document.createElement('div');
 			div.onclick = function() {modDiv(this)};
-			div.style.border = '0';
 			div.draggable = "true";
 			div.id = 'div'+id;
 			id+=1;
+			div.style.position = 'relative';
 			div.style.float = 'left';
 			div.style.resize = 'both';
 			div.style.overflow = 'hidden';
 			div.style.height = '50px';
 			div.style.width = '50px';
+			div.style.border = '0';
 			div.style.background = 'green';
 			
 			var button = document.createElement("button");
@@ -82,12 +155,15 @@
         };
 
         document.getElementById('li').onclick = function() {
+		    document.getElementById('header2').style.display = 'block';
+			document.getElementById('header3').style.display = 'none';
 			var main = document.getElementById('main');
 			var div  = document.createElement('div');
 			div.onmouseout = "this.style.border=0";
 			div.draggable = "true";
 			div.id = 'div'+id;
 			id+=1;
+			div.style.position = 'relative';
 			div.style.float = 'left';
 				var ul  = document.createElement('ul');
 				ul.id = 'div'+idUl;
@@ -134,7 +210,42 @@
 						li3.appendChild(a3);
 					ul.appendChild(li3);
 			div.appendChild(ul);
+			
+			var button = document.createElement("button");
+			button.className = 'borrar';
+			button.onclick = function() {
+				delDiv(this);
+			};
+
+			div.appendChild(button);
 			main.appendChild(div);
+        };
+		
+		document.getElementById('img').onclick = function() {
+		    document.getElementById('header2').style.display = 'none';
+			document.getElementById('header3').style.display = 'block';
+			var main = document.getElementById('main');
+			var div  = document.createElement('div');
+			div.onclick = function() {modImg(this)};
+			div.draggable = "true";
+			div.id = 'div'+id;
+			id+=1;
+			div.style.position = 'relative';
+			div.style.float = 'left';
+			div.style.resize = 'both';
+			div.style.overflow = 'hidden';
+			div.innerHTML = '<img draggable="false" style="width: 50px; height: 50px" src="https://www.google.com/images/srpr/logo11w.png" />';
+			 
+			var button = document.createElement("button");
+			button.className = 'borrar';
+			button.onclick = function() {
+				delDiv(this);
+			};
+
+			div.appendChild(button);
+
+			main.appendChild(div);
+			
         };
     };
 })();
